cedc9c7f507c00821cc47541680a9440ce52575a
chipi <chipineiro@gmail.com>
2025-02-15 23:50:34 -0600

    Add forms, urls and views for question

---
+++ questions/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.question_list, name='question_list'),
+    path('question/<int:pk>/', views.question_detail, name='question_detail'),
+    path('question/new/', views.question_new, name='question_new'),
+    path('question/<int:pk>/edit/', views.question_edit, name='question_edit'),
+    path('question/<int:pk>/delete/', views.question_delete, name='question_delete'),
+]
