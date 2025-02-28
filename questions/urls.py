c53ac18f962ced703ff7a4cb1c2c0a1bf982402f
chipi <chipineiro@gmail.com>
2025-02-27 22:37:13 -0600

    Create questions lists

--- questions/urls.py
+++ questions/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from .views import QuestionViewForm
+from .views import QuestionListView, QuestionViewForm
 
 urlpatterns = [
+    path('', QuestionListView.as_view(), name='list_questions'),
     path('agregar/', QuestionViewForm.as_view(), name='agregar'),
 ]
--- questions/views.py
+++ questions/views.py
@@ -1,11 +1,20 @@
 from django.views import generic
+
+from questions.models import Question
 from .forms import QuestionForm
+
 
 class QuestionViewForm(generic.FormView):
     form_class = QuestionForm
-    template_name = 'questions/add_question.html'
+    template_name = "questions/add_question.html"
 
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
 
+
+class QuestionListView(generic.ListView):
+    model = Question
+    template_name = "questions/list_questions.html"
+    context_object_name = "questions"
+    paginate_by = 10
