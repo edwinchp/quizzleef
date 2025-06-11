c1cb0aeca52a62594bf552f15c83e3075eea0359
chipi <chipineiro@gmail.com>
2025-06-11 16:06:29 -0600

    Update readme and env to improve installation

--- questions/models.py
+++ questions/models.py
@@ -6,11 +6,12 @@ class Question(models.Model):
     photo = models.FileField(null=True, upload_to='questions/pictures', blank=True)
     photo_caption = models.TextField(max_length=300, blank=True, null=True, help_text="Max characters: 300")
     photo_spoiler = models.BooleanField(blank=True, default=False, choices=[(True, 'Yes'), (False, 'No')], help_text="Spoiler text for the photo")
-    dificulty = models.CharField(max_length=10, choices=[('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')], default='easy', help_text="Select difficulty level")
+    difficulty = models.CharField(max_length=10, choices=[('easy', 'Easy'), ('medium', 'Medium'), ('hard', 'Hard')], default='easy', help_text="Select difficulty level")
     hint = models.TextField(blank=True, null=True, help_text="Hint for the question")
 
     def __str__(self):
         return self.question_text
+
 
 class Option(models.Model):
     question = models.ForeignKey(Question, on_delete=models.DO_NOTHING, related_name="options")
@@ -19,10 +20,17 @@ class Option(models.Model):
 
     def __str__(self):
         return f"{self.is_correct}: {self.option_text}"
-    
+
+
 class Message(models.Model):
     question = models.ForeignKey(Question, on_delete=models.DO_NOTHING, related_name="messages")
     message_text = models.TextField(max_length=500, help_text="Max characters: 500")
 
     def __str__(self):
         return f"{self.message_text}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, help_text="Max characters: 30")
+    def __str__(self):
+        return self.name
--- readme.md
+++ readme.md
@@ -12,10 +12,18 @@ A simple Django-based Question Manager application created as part of my Python/
 - (Future) AI Integration analizing score and as a study buddy
 
 ## Installation
-- Define environment variables
+Define environment variables
 ```bash
 cp .env_example .env
 ```
+
+Replace .env with your information
+
+Set environment variables (not recommended for Production)
+```bash
+export $(grep -v '^#' .env | xargs)
+```
+
 Install requirements
 ```bash
 pip install -r requirements.txt
