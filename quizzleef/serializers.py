82018aa92ad3c81b79959fa7609a18b136448045
chipi <chipineiro@gmail.com>
2025-07-23 22:08:33 -0600

    Add options to question

--- quizzleef/serializers.py
+++ quizzleef/serializers.py
@@ -1,7 +1,16 @@
 from rest_framework import serializers
-from questions.models import Question
+from questions.models import Question, Option
+
+class OptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Option
+        fields = ['id', 'option_text', 'is_correct']
+        read_only_fields = ['id', 'option_text', 'is_correct']
 
 class QuestionSerializer(serializers.ModelSerializer):
+    options = OptionSerializer(many=True, read_only=True)
+    
     class Meta:
         model = Question
-        fields = '__all__'
+        fields = ['id', 'question_text', 'short_explanation', 'photo', 'photo_caption', 
+                 'photo_spoiler', 'difficulty', 'hint', 'category', 'options']
